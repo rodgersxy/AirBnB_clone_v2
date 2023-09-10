035fe21404341fa14bd5c679246f6766f540b3be
rodgersxy <rodgersny99@gmail.com>
2023-09-09 23:03:39 -0700

    update puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,7 +10,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://github.com/besthor/;
+        return 301 http://github.com/rodgersxy/;
     }
     error_page 404 /404.html;
     location /404 {
